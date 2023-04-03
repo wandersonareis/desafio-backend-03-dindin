dfb80f5242ea5a46b468f6153331e2661c741c80
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2023-04-03 01:20:24 -0300

    Refatorado userMiddleware

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -1,21 +1,21 @@
 const knex = require("../db/dbClient");
-const { emailExist, findUserByEmail } = require("../services/user.service");
-
-const { passwordIsNotValid, passwordExclude } = require("../utils/passwordsUtil");
+const { passwordIsNotValid } = require("../utils/passwordsUtil");
 const ErrorHandler = require("./errorHandling/errorHandler.class");
 
 async function userLoginValidate(req, res, next) {
   const { email: body_email, senha: body_password } = req.body;
   const userInvalidMsg = "Usuário e/ou senha inválido(s).";
   try {
-    const user = await findUserByEmail(body_email);
+    const user = await knex("usuarios").where({ email: body_email }).first();
 
     if (!user) throw new ErrorHandler(userInvalidMsg, 404);
 
-    const isNotValid = await passwordIsNotValid(body_password, user.senha);
+    const { senha: user_password, ...userDto } = user;
+    
+    const isNotValid = await passwordIsNotValid(body_password, user_password);
     if (isNotValid) throw new ErrorHandler(userInvalidMsg, 400);
 
-    req.user = passwordExclude(user);
+    req.user = userDto;
     next();
   } catch (error) {
     next(error);
@@ -23,15 +23,18 @@ async function userLoginValidate(req, res, next) {
 }
 
 async function userCommonMiddleware(req, res, next) {
-  const body_email = req.body?.email;
+  const body_email = req.body.email;
   const user_email = req.user?.email;
-  try {
-    if (user_email && user_email !== body_email) {
-      const emailExists = await emailExist(body_email);
 
-      if (emailExists) {
-        throw new ErrorHandler("O e-mail informado já está sendo utilizado por outro usuário.", 401);
-      }
+  const emailExists = await knex("usuarios").where({ email: body_email }).first();
+
+  try {
+    if (emailExists && user_email !== body_email) {
+      throw new ErrorHandler("O e-mail informado já está sendo utilizado por outro usuário.", 401);
+    }
+    
+    if (emailExists && !user_email) {
+      throw new ErrorHandler("O e-mail informado já está sendo utilizado por outro usuário.", 401);
     }
 
     next();
